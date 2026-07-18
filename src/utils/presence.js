45e99e08e0c08e3ef9e958dceb93ddf0e384ab0f
ejayontop <roseteejay1@gmail.com>
2026-07-18 22:59:50 +0800

    Update presence.js

--- src/utils/presence.js
+++ src/utils/presence.js
@@ -1,15 +1,19 @@
 import config from '../config/application.js';
+import { getActiveCount } from '../services/activeScriptService.js';
 
 export function buildPresenceActivities(client) {
   const memberCount = client.guilds.cache.reduce(
     (total, guild) => total + guild.memberCount,
     0
   );
-  const formatted = memberCount.toLocaleString();
+  const formattedMembers = memberCount.toLocaleString();
+  const formattedActive = getActiveCount().toLocaleString();
 
   return config.bot.presence.activities.map((activity) => ({
     ...activity,
-    name: activity.name.replace('{memberCount}', formatted),
+    name: activity.name
+      .replace('{memberCount}', formattedMembers)
+      .replace('{activePlayers}', formattedActive),
   }));
 }
 
