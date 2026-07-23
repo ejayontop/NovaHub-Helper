831dafe8a4b646818579a858ecd80307238ecbd7
ejayontop <roseteejay1@gmail.com>
2026-07-23 20:00:15 +0800

    Create scriptpanel.js

---
+++ src/commands/scriptpanel.js
@@ -0,0 +1,21 @@
+import { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
+import { getColor } from '../config/bot.js';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('scriptpanel')
+    .setDescription('Post the Get Script panel'),
+
+  async execute(interaction) {
+    const embed = new EmbedBuilder()
+      .setColor(getColor('primary'))
+      .setTitle('Get the Script')
+      .setDescription('Click below to get the script.');
+
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder().setCustomId('get_script').setLabel('Get Script').setStyle(ButtonStyle.Primary),
+    );
+
+    await interaction.reply({ embeds: [embed], components: [row] });
+  },
+};
