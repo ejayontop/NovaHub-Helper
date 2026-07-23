a36d3801c4c0dba09d010a906a686cf5e4fef617
ejayontop <roseteejay1@gmail.com>
2026-07-23 20:06:21 +0800

    Update scriptPanel.js

--- src/interactions/buttons/scriptPanel.js
+++ src/interactions/buttons/scriptPanel.js
@@ -7,26 +7,23 @@ async function replyThenDelete(interaction, content) {
   }, EPHEMERAL_LIFETIME_MS);
 }
 
-const handlers = {
-  get_script: async (interaction) => {
-    await replyThenDelete(
-      interaction,
-      'loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/novahub.lua"))()'
-    );
+export default [
+  {
+    name: 'get_script',
+    async execute(interaction) {
+      await replyThenDelete(
+        interaction,
+        'loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaHubRBLX/NovaHub/refs/heads/main/novahub.lua"))()'
+      );
+    },
   },
-
-  get_free_key: async (interaction) => {
-    await replyThenDelete(
-      interaction,
-      'NOVAHUB_8f2kx7me'
-    );
+  {
+    name: 'get_free_key',
+    async execute(interaction) {
+      await replyThenDelete(
+        interaction,
+        'NOVAHUB_8f2kx7me'
+      );
+    },
   },
-};
-
-export default {
-  customIds: ['get_script', 'get_free_key'],
-  async execute(interaction) {
-    const handler = handlers[interaction.customId];
-    if (handler) await handler(interaction);
-  },
-};
+];
