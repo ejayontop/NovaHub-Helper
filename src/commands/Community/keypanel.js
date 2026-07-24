02019354a0e77dc284c297a074d6dcec9bad0e19
ejayontop <roseteejay1@gmail.com>
2026-07-24 11:40:05 +0800

    Update keypanel.js

--- src/commands/Community/keypanel.js
+++ src/commands/Community/keypanel.js
@@ -11,9 +11,9 @@ export default {
   async execute(interaction) {
     const embed = new EmbedBuilder()
       .setColor(getColor('brand'))
-      .setTitle('400+ Members FREE Premium Key by 7 Days (Free) @vlan.bin')
+      .setTitle('1K+ Members FREE Premium Key by 3 Days (Free)')
        .setImage('https://raw.githubusercontent.com/ejayontop/NovaHub-Helper/refs/heads/main/src/commands/Community/novahubbanner.png')
-      .setDescription('New Expiration Date: July 24, 2026');
+      .setDescription('New Expiration Date: July 27, 2026');
 
     const row = new ActionRowBuilder().addComponents(
       new ButtonBuilder().setCustomId('get_free_key').setLabel('Get Key').setStyle(ButtonStyle.Success),
